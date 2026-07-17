b657dac2973bb1b1a40855ccda8b26e3c9a72f0d
zerocker <zerocloude@gmail.com>
2026-07-17 23:26:40 +0900

    Inline the Layout asset to Hud for now

--- src/Moggy/Hud.cs
+++ src/Moggy/Hud.cs
@@ -17,12 +17,9 @@ public sealed class HudSystem : GameSystem
 
     private FontAsset _font = null!;
 
-    private Layout _layout = null!;
-
     public override void Startup()
     {
         _font = Assets.Load<FontAsset>("Fonts/JoystixMonospace");
-        _layout = Assets.LoadJson<Layout>("Hud/Layout");
     }
 
     public override void Render(Time time)
@@ -31,24 +28,24 @@ public sealed class HudSystem : GameSystem
         {
             if (Game.State == GameState.Paused)
             {
-                Batcher.TextMonospaced(_font.Sprite, Paused, _layout.PausedPosition, Color.Red);
+                Batcher.TextMonospaced(_font.Sprite, Paused, Layout.PausedPosition, Color.Red);
             }
 
-            Batcher.TextMonospaced(_font.Sprite, PlayerOneLabel, _layout.PlayerOnePosition, _layout.PlayerOneColor);
-            Batcher.TextMonospaced(_font.Sprite, Score, _layout.PlayerOneScorePosition, Color.White);
-            Batcher.TextMonospaced(_font.Sprite, PlayerTwoLabel, _layout.PlayerTwoPosition, _layout.PlayerTwoColor);
-            Batcher.TextMonospaced(_font.Sprite, Score, _layout.PlayerTwoScorePosition, Color.White);
+            Batcher.TextMonospaced(_font.Sprite, PlayerOneLabel, Layout.PlayerOnePosition, Layout.PlayerOneColor);
+            Batcher.TextMonospaced(_font.Sprite, Score, Layout.PlayerOneScorePosition, Color.White);
+            Batcher.TextMonospaced(_font.Sprite, PlayerTwoLabel, Layout.PlayerTwoPosition, Layout.PlayerTwoColor);
+            Batcher.TextMonospaced(_font.Sprite, Score, Layout.PlayerTwoScorePosition, Color.White);
         });
     }
 
-    [UsedImplicitly]
-    private record Layout(
-        Vector2 PausedPosition,
-        Vector2 PlayerOnePosition,
-        Vector2 PlayerTwoPosition,
-        Vector2 PlayerOneScorePosition,
-        Vector2 PlayerTwoScorePosition,
-        Color PlayerOneColor,
-        Color PlayerTwoColor
-    );
+    private static class Layout
+    {
+        public static readonly Vector2 PausedPosition = new(96, 152);
+        public static readonly Vector2 PlayerOnePosition = new(8, 0);
+        public static readonly Vector2 PlayerTwoPosition = new(184, 0);
+        public static readonly Vector2 PlayerOneScorePosition = new(8, 8);
+        public static readonly Vector2 PlayerTwoScorePosition = new(184, 8);
+        public static readonly Color PlayerOneColor = Color.FromHexStringRGB("#00BFFF");
+        public static readonly Color PlayerTwoColor = Color.FromHexStringRGB("#800080");
+    }
 }
